Wah68/blockscout

chore: Reindex incorrect internal transactions migration

apps/explorer/config/config.exs
@@ -123,6 +123,7 @@ config :explorer, Explorer.Migrator.SanitizeIncorrectWETHTokenTransfers, enabled
 config :explorer, Explorer.Migrator.TransactionBlockConsensus, enabled: true
 config :explorer, Explorer.Migrator.TokenTransferBlockConsensus, enabled: true
 config :explorer, Explorer.Migrator.RestoreOmittedWETHTransfers, enabled: true
+config :explorer, Explorer.Migrator.ReindexIncorrectInternalTransactions, enabled: true
 
 config :explorer, Explorer.Chain.Fetcher.CheckBytecodeMatchingOnDemand, enabled: true
 

apps/explorer/config/runtime/test.exs
@@ -48,6 +48,7 @@ config :explorer, Explorer.Migrator.SanitizeIncorrectWETHTokenTransfers, enabled
 config :explorer, Explorer.Migrator.TransactionBlockConsensus, enabled: false
 config :explorer, Explorer.Migrator.TokenTransferBlockConsensus, enabled: false
 config :explorer, Explorer.Migrator.RestoreOmittedWETHTransfers, enabled: false
+config :explorer, Explorer.Migrator.ReindexIncorrectInternalTransactions, enabled: false
 
 config :explorer,
   realtime_events_sender: Explorer.Chain.Events.SimpleSender

apps/explorer/lib/explorer/application.ex
@@ -143,7 +143,8 @@ defmodule Explorer.Application do
         configure(Explorer.Migrator.TokenTransferBlockConsensus),
         configure(Explorer.Migrator.RestoreOmittedWETHTransfers),
         configure_chain_type_dependent_process(Explorer.Chain.Cache.StabilityValidatorsCounters, :stability),
-        configure_mode_dependent_process(Explorer.Migrator.ShrinkInternalTransactions, :indexer)
+        configure_mode_dependent_process(Explorer.Migrator.ShrinkInternalTransactions, :indexer),
+        configure_mode_dependent_process(Explorer.Migrator.ReindexIncorrectInternalTransactions, :indexer)
       ]
       |> List.flatten()
 

quentin365/blockscout-L2

apps/explorer/config/config.exs
@@ -123,6 +123,7 @@ config :explorer, Explorer.Migrator.SanitizeIncorrectWETHTokenTransfers, enabled
 config :explorer, Explorer.Migrator.TransactionBlockConsensus, enabled: true
 config :explorer, Explorer.Migrator.TokenTransferBlockConsensus, enabled: true
 config :explorer, Explorer.Migrator.RestoreOmittedWETHTransfers, enabled: true
+config :explorer, Explorer.Migrator.ReindexInternalTransactionsWithIncompatibleStatus, enabled: true
 
 config :explorer, Explorer.Chain.Fetcher.CheckBytecodeMatchingOnDemand, enabled: true
 

apps/explorer/config/runtime/test.exs
@@ -48,6 +48,7 @@ config :explorer, Explorer.Migrator.SanitizeIncorrectWETHTokenTransfers, enabled
 config :explorer, Explorer.Migrator.TransactionBlockConsensus, enabled: false
 config :explorer, Explorer.Migrator.TokenTransferBlockConsensus, enabled: false
 config :explorer, Explorer.Migrator.RestoreOmittedWETHTransfers, enabled: false
+config :explorer, Explorer.Migrator.ReindexInternalTransactionsWithIncompatibleStatus, enabled: false
 
 config :explorer,
   realtime_events_sender: Explorer.Chain.Events.SimpleSender

apps/explorer/lib/explorer/application.ex
@@ -143,7 +143,8 @@ defmodule Explorer.Application do
         configure(Explorer.Migrator.TokenTransferBlockConsensus),
         configure(Explorer.Migrator.RestoreOmittedWETHTransfers),
         configure_chain_type_dependent_process(Explorer.Chain.Cache.StabilityValidatorsCounters, :stability),
-        configure_mode_dependent_process(Explorer.Migrator.ShrinkInternalTransactions, :indexer)
+        configure_mode_dependent_process(Explorer.Migrator.ShrinkInternalTransactions, :indexer),
+        configure_mode_dependent_process(Explorer.Migrator.ReindexInternalTransactionsWithIncompatibleStatus, :indexer)
       ]
       |> List.flatten()
 